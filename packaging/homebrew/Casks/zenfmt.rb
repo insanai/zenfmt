c0c3234b8c3e7523c925d67e2a9a1da2aacce82b
Vikrant Rathore <admin@vyomtech.com>
2026-08-12 15:24:13 +0800

    homebrew: update cask to 0.3.5

--- packaging/homebrew/Casks/zenfmt.rb
+++ packaging/homebrew/Casks/zenfmt.rb
@@ -1,9 +1,9 @@
 cask "zenfmt" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "0.3.3"
-  sha256 arm:   "c12d137bb4a61f75d484c3d8747bf5f9681057c6b4093600d37834a6b0e10f99",
-         intel: "2a923e9027717df05f4410b00f1ee237292b2fa9b67dbd65d9c1a75787c3776f"
+  version "0.3.5"
+  sha256 arm:   "3dcad02dfdf1cf985cb62a5ba1084a0a546bc71569d8d58ee5fab642949bfd2a",
+         intel: "49438c9ad16e3027a11b5af5e3f1b3722e8ed395daf0d731328bd64183e9d84e"
 
   url "https://github.com/insanai/zenfmt/releases/download/v#{version}/zenfmt-#{version}-#{arch}-macos.tar.gz"
   name "zenfmt"
